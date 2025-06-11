67200418d634441fc6f0de522ea674c4c3601fc8
Babu Macharia <babu.macharia@gmail.com>
2025-06-11 20:53:14 +0000

    feat(backend): added container image building and pushing

--- src/backend/src/backend/main.py
+++ src/backend/src/backend/main.py
@@ -98,6 +98,8 @@ async def read_stats():
 
 # Marker for CI pipeline
 # This comment is used to trigger the CI pipeline when changes are made to this file.
+# This comment is ysed to trigger the CI pipeline when changes are made to this file
+# This comment is used to trigger the CI pipeline when changes are made to this file.
 
 
 def main():
