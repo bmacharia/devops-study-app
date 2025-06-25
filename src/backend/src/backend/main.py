bffc73053c479b215d2ba775e0adc5b1fa5123fa
Babu Macharia <babu.macharia@gmail.com>
2025-06-25 18:53:04 +0000

    fix(backend): trigger CI 2

--- src/backend/src/backend/main.py
+++ src/backend/src/backend/main.py
@@ -102,6 +102,7 @@ async def read_stats():
 # This comment is used to trigger the CI pipeline when changes are made to this file.
 # This comment is used to trigger the CI pipeline when changes are made to this file.
 # This comment will trigger CI pipeline
+# This comment will triggger a CI pipeline
 
 
 def main():
